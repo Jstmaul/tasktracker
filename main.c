a4f9ad8998f5ae2832a8be2c2ac0ea9492fd1cd6
jstmaul <maunadhif1@gmail.com>
2024-09-01 20:28:06 +0700

    usual commit

--- main.c
+++ main.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
-static char* user_i;
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    if (!strcmp(argv[1], "list")) {
+      if (argc > 2) {
+        if (!strcmp(argv[2], "done")) {
+        } else if (!strcmp(argv[2], "in-progress")) {
+        }
+      } else {
+        FILE *fptr;
 
-int main() {
-  fgets(user_i, sizeof(user_i), stdin);
-  switch (*user_i) {
-    case '1':
-      printf("done");
-      break;
-    case 'add':
-    printf("what add?");
-    break;
-    default:
-      printf("please input something\n");
-      break;
+      read_to_do_list:
+
+        fptr = fopen("to_do_list.txt", "r");
+
+        if (fptr == NULL) {
+          printf("No file detected %s\n", "Creating to_do_list.txt\n");
+
+          fptr = fopen("to_do_list.txt", "w");
+
+          goto read_to_do_list;
+
+        } else {
+          char file_buffer[sizeof(*fptr)];
+
+          if (fgets(file_buffer, sizeof(file_buffer), fptr)) {
+            printf("%s", file_buffer);
+          }
+        }
+
+        fclose(fptr);
+      }
+    } else if (!strcmp(argv[1], "add")) {
+    } else if (!strcmp(argv[1], "delete")) {
+    } else if (!strcmp(argv[1], "update")) {
+    } else if (!strcmp(argv[1], "mark-done")) {
+    } else if (!strcmp(argv[1], "mark-in-progress")) {
+    } else {
+      printf("no valid input\n");
+    }
   }
+  return 0;
 }
